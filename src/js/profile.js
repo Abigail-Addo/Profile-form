fc46ef3302c2ad2fd260ebd37ecb83646d084db3
Abigail <addoa350@gmail.com>
2023-07-22 21:20:46 +0000

    profile-form

--- src/js/profile.js
+++ src/js/profile.js
@@ -32,7 +32,7 @@ $(function () {
     const logout = document.querySelector(".logout");
     logout.addEventListener("click", function (e) {
         e.preventDefault();
-        window.location.href = "../Log in/index.html";
+        window.location.href = "log-in.html";
     });
 
     console.log("main javascript file is working")
